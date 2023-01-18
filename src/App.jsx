460e8bf6bdb28fc9d113a63bc87b756ef56d8a25
Mathanstunnerz <mathanstunnerz95@gmail.com>
2023-01-18 15:26:48 +0530

    day-23 react

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+     
+        <div className="main_body1 ">
+        <div className="main_body">
+            <div className="row">
+                <div className="col-4">
+                <div className="card">
+                    <div className=" container">
+                    <h5 className="text-center ">Free</h5>
+                        <h1 className="text-center">$0<span className="text">/month</span></h1>
+                        <hr/>
+                        <ul className="fa-ul">
+                            <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
+                            <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
+                            <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+                            <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+                            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
+                              Private Projects</li>
+                            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
+                              Phone Support</li>
+                            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain
+                            </li>
+                            <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
+                              Reports</li>
+                          </ul>
+                          <div className="button container"><a href='#'className='a'>click</a></div>
+                        </div>
+                  </div>
+                </div>
+                <div className="col-4">
+                <div className="card" >
+                    <div className=" container">
+                    <h5 className="text-center ">Plus</h5>
+                        <h1 className="text-center">$9<span className="text">/month</span></h1>
+                        <hr/>
+                        <ul className="fa-ul">
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>5 Users</strong></li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Free Subdomain</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
+                Reports</li>
+            </ul>
+                          <div className="button container"><a href='#'className='a'>click</a></div>
+                        </div>
+                  </div>
+                </div>
+                <div className="col-4">
+                <div className="card" >
+                    <div className=" container">
+                    <h5 className="text-center ">Pro</h5>
+                        <h1 className="text-center">$49<span className="text">/month</span></h1>
+                        <hr/>
+                        <ul className="fa-ul">
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited Users</strong>
+              </li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited</strong> Free
+                Subdomains</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span>Monthly Status Reports</li>
+            </ul>
+                          <div className="button container"><a href='#' className='a'>click</a></div>
+                        </div>
+                  </div>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
